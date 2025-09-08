228b34883a75c21fc4764a243b80423e65b47717
DoHieu <dohieu01642@gmail.com>
2025-09-08 22:58:08 +0700

    update port

--- server.js
+++ server.js
@@ -665,4 +665,8 @@ function pushState(userId) {
 setInterval(() => { for (const uid of sockets.keys()) pushState(uid); }, 3000);
 
 // ---- Start ----
-server.listen(3000, () => console.log('Game running on http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log('Game running on port ' + PORT);
+});
+
